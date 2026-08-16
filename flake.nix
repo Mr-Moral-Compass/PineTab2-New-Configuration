6093f147df5b5c331a0c45efae97e44c88c0f0f4
Mr-Moral-Compass <ransolski@gmail.com>
2026-08-15 21:15:53 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -10,6 +10,18 @@
     };
   };
 
+  # Use the binary cache provided by nixos-rockchip.
+  # This is especially important for the PineTab2 kernel.
+  nixConfig = {
+    extra-substituters = [
+      "https://nabam-nixos-rockchip.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "nabam-nixos-rockchip.cachix.org-1:BQDltcnV8GS/G86tdvjLwLFz1WeFqSk7O9yl+DR0AVM="
+    ];
+  };
+
   outputs = { self, nixpkgs, nixos-rockchip, ... }:
     let
       system = "aarch64-linux";
@@ -19,8 +31,12 @@
         config.allowUnfree = true;
       };
 
+      # PineTab2-specific Linux 6.18 kernel.
+      #
+      # This is the current PineTab2 kernel provided by
+      # nixos-rockchip and is based on DanctNIX linux-pinetab2.
       pinetabKernel =
-        nixos-rockchip.legacyPackages.${system}.kernel_linux_7_0_pinetab_unstable;
+        nixos-rockchip.legacyPackages.${system}.kernel_linux_6_18_pinetab_stable;
 
     in
     {
@@ -28,6 +44,7 @@
         inherit system;
 
         modules = [
+          # PineTab2 hardware support.
           nixos-rockchip.nixosModules.dtOverlayPineTab2
           nixos-rockchip.nixosModules.bes2600
           nixos-rockchip.nixosModules.noZFS
@@ -35,23 +52,58 @@
           {
             nixpkgs.hostPlatform = system;
 
+            # PineTab2-specific kernel.
             boot.kernelPackages = pinetabKernel;
 
-            boot.kernelParams = [
-              "root=/dev/mmcblk1p2"
-              "rw"
-              "rootwait"
-            ];
+            # ------------------------------------------------------------
+            # Existing PineTab2 SD-card filesystem
+            # ------------------------------------------------------------
 
-            system.stateVersion = "26.05";
+            fileSystems."/" = {
+              device = "/dev/disk/by-uuid/72475447-7667-4f89-bc4a-fd19046236b3";
+              fsType = "ext4";
+            };
+
+            fileSystems."/boot" = {
+              device = "/dev/disk/by-uuid/14D2-6235";
+              fsType = "vfat";
+            };
+
+            # The SD image already has PineTab2 U-Boot.
+            #
+            # Do NOT install GRUB.
+            # Generate an extlinux configuration that the existing
+            # U-Boot can consume.
+            boot.loader.grub.enable = false;
+            boot.loader.generic-extlinux-compatible.enable = true;
+
+            # This is an existing installation originally based on the
+            # older Asonix/PineTab2 configuration.  Keep its state version
+            # independent of the current nixpkgs release.
+            system.stateVersion = "23.05";
+
+            # ------------------------------------------------------------
+            # System
+            # ------------------------------------------------------------
 
             networking.hostName = "pinetab2";
+
             networking.networkmanager.enable = true;
+
+            # ------------------------------------------------------------
+            # Desktop
+            # ------------------------------------------------------------
 
             services.xserver.enable = true;
 
             services.xserver.desktopManager.gnome.enable = true;
             services.xserver.displayManager.gdm.enable = true;
+
+            environment.variables.MOZ_ENABLE_WAYLAND = "1";
+
+            # ------------------------------------------------------------
+            # Audio
+            # ------------------------------------------------------------
 
             services.pipewire = {
               enable = true;
@@ -62,7 +114,12 @@
 
             security.rtkit.enable = true;
 
+            # ------------------------------------------------------------
+            # Hardware / desktop services
+            # ------------------------------------------------------------
+
             services.geoclue2.enable = true;
+
             services.automatic-timezoned.enable = true;
 
             services.flatpak.enable = true;
@@ -75,12 +132,18 @@
               openFirewall = true;
             };
 
-            environment.variables.MOZ_ENABLE_WAYLAND = "1";
+            # ------------------------------------------------------------
+            # Packages
+            # ------------------------------------------------------------
 
             environment.systemPackages = with pkgs; [
               firefox
               htop
             ];
+
+            # ------------------------------------------------------------
+            # Nix
+            # ------------------------------------------------------------
 
             nix.settings.experimental-features = [
               "nix-command"
