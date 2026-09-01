51de5e8466dde297825315e2c7f7d8b559f0bee1
Mr-Moral-Compass <ransolski@gmail.com>
2026-09-01 18:48:33 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,154 +1,52 @@
 {
+  description = "Build example NixOS image for PineTab2";
+
   inputs = {
-    nixpkgsStable.url = "nixpkgs/nixos-26.05";
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    rockchip.url = "github:nabam/nixos-rockchip";
-  };
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-26.05";
+    nixpkgsUnstable.url = "nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, rockchip, ... }:
-  let
-    system = "aarch64-linux";
-    hostname = "PineTab2";
-    username = "pinetab2";
-    initialPassword = "changeme";
 
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-
-    buildNixosConfiguration = { kernel, uBoot }: nixpkgs.lib.nixosSystem {
-      inherit system;
-
-      modules = [
-        rockchip.nixosModules.sdImageRockchip
-        rockchip.nixosModules.dtOverlayPCIeFix
-
-        # pinetab2 cachix
-        # Use cache with packages from nabam/nixos-rockchip CI.
-        {
-          nix = {
-            settings = {
-              substituters = [
-                "https://pinetab2.cachix.org"
-                "https://nabam-nixos-rockchip.cachix.org"
-              ];
-              trusted-public-keys = [
-                "pinetab2.cachix.org-1:q3+zliGsfh1MH76ugM2GkPQcO2nALvM3sDSS/dXnxcE="
-                "nabam-nixos-rockchip.cachix.org-1:BQDltcnV8GS/G86tdvjLwLFz1WeFqSk7O9yl+DR0AVM"
-              ];
-            };
-          };
-        }
-
-        ({ pkgs, lib, ... }: {
-          # Ensure we don't try building zfs modules for the kernel (they're broken)
-          nixpkgs.overlays = [
-            (final: super: {
-              zfs = super.zfs.overrideAttrs (_: {
-                meta.platforms = [ ];
-              });
-            })
-          ];
-
-          system.stateVersion = "23.11";
-
-          users.users.${username} = {
-            inherit initialPassword;
-            isNormalUser = true;
-            extraGroups = [ "wheel" "networkmanager" ];
-          };
-
-          rockchip.uBoot = (rockchip.uBoot system).uBootPineTab2;
-
-          boot.kernelPackages = kernel;
-          boot.kernelParams = [ "console=ttyS2,1500000n8" "rootwait" "root=LABEL=NIXOS_SD" "rw" ];
-
-          networking.networkmanager.enable = true;
-
-          services = {
-            xserver = {
-              enable = true;
-              desktopManager.gnome.enable = true;
-              displayManager.gdm.enable = true;
-            };
-
-            automatic-timezoned.enable = true;
-            geoclue2.enableDemoAgent = lib.mkForce true;
-
-            flatpak.enable = true;
-            printing.enable = true;
-
-            programs.appimage.enable = true;
-            programs.appimage.binfmt = true;
-
-            avahi = {
-              enable = true;
-              openFirewall = true;
-            };
-
-            pipewire = {
-              enable = true;
-              alsa = {
-                enable = true;
-                support32Bit = true;
-              };
-              pulse.enable = true;
-              jack.enable = true;
-            };
-          };
-
-          #sound.enable = true;
-          hardware.pulseaudio.enable = false;
-          security.rtkit.enable = true;
-
-          environment.systemPackages = with pkgs; [
-            cachix
-            firefox
-            gnomeExtensions.arc-menu
-            gnomeExtensions.dash-to-dock
-            gnomeExtensions.dash-to-panel
-            gnomeExtensions.gjs-osk
-            gnomeExtensions.one-window-wonderland
-            htop
-          ];
-
-          environment.sessionVariables = {
-            MOZ_ENABLE_WAYLAND = "1";
-          };
-
-          networking.hostName = "${hostname}";
-                nixPath = [
-        "nixpkgs=${inputs.nixpkgs}"
-      ];
-    
-          nix.optimise.automatic = true;
-    
-          nix.gc = {
-            automatic = true;
-            dates = "weekly";
-            options = "--delete-older-than 7d";
-          };
-
-          nix.settings = {
-            experimental-features = [ "nix-command" "flakes" ];
-          };
-        })
-      ];
-    };
-  in
-  {
-    nixosConfigurations.${hostname} = buildNixosConfiguration {
-      # use a custom kernel for next rebuild (we have cachix now)
-      kernel = inputs.rockchip.legacyPackages.${buildPlatform}.kernel_linux_latest_rockchip_stable;
-      # kernel = pkgs.linuxPackages_latest;
-      # todo: uboot isn't required after we're already booting
-      uBoot = inputs.rockchip.packages.${buildPlatform}.uBootPineTab2;
-    };
-    nixosConfigurations.nixos = buildNixosConfiguration {
-      # use a prebuilt kernel for first rebuild
-      kernel = pkgs.linuxPackages_latest;
-      # todo: uboot isn't required after we're already booting
-      uBoot = (rockchip.uBoot system).uBootPineTab2;
+    rockchip = {
+      url = "github:nabam/nixos-rockchip";
     };
   };
+
+  # Use cache with packages from nabam/nixos-rockchip CI.
+  nixConfig = {
+    extra-substituters = [ "https://nabam-nixos-rockchip.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nabam-nixos-rockchip.cachix.org-1:BQDltcnV8GS/G86tdvjLwLFz1WeFqSk7O9yl+DR0AVM"
+    ];
+  };
+
+  outputs =
+    { self, ... }@inputs:
+    let
+      osConfig =
+        buildPlatform:
+        inputs.nixpkgs.lib.nixosSystem {
+          system = "aarch64-linux";
+          modules = [
+            inputs.rockchip.nixosModules.sdImageRockchip
+            inputs.rockchip.nixosModules.dtOverlayQuartz64ASATA
+            inputs.rockchip.nixosModules.dtOverlayPCIeFix
+            inputs.rockchip.nixosModules.noZFS
+            ./config.nix
+            {
+              # Use cross-compilation for uBoot and Kernel.
+              rockchip.uBoot = inputs.rockchip.packages.${buildPlatform}.uBootPineTab2;
+              boot.kernelPackages =
+                inputs.rockchip.legacyPackages.${buildPlatform}.kernel_linux_latest_rockchip_stable;
+            }
+          ];
+        };
+    in
+    {
+      nixosConfigurations.PineTab2 = osConfig "aarch64-linux";
+    }
+    // inputs.utils.lib.eachDefaultSystem (system: {
+      packages.image = (osConfig system).config.system.build.sdImage;
+      packages.default = self.packages.${system}.image;
+    });
 }
