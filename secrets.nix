c50d031a246849733d1d8267955141b45351372e
Mr-Moral-Compass <ransolski@gmail.com>
2026-09-01 19:53:46 -0400

    Create secrets.nix

---
+++ secrets.nix
@@ -0,0 +1,7 @@
+{
+  username = "pinetab2";
+  initialPassword = "changeme";
+  authorizedKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMpM+jZrz5f2imQD71+pPmvDM5aGSyz/5VJH61g4Q9SF raboof@bzzt.net";
+  ssid = "FRITZ!Box 7581 TN";
+  psk = "00560943358746562644";
+}
